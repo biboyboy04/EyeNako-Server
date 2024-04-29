0f20ac8872acff1679373e58db9f5cae1108dccb
biboyboy04 <55369988+biboyboy04@users.noreply.github.com>
2024-04-29 17:56:41 +0800

    Setup DB Connection
    - Open db connection with config using the env variables
    
    - Pass the db to the server as we have a db connection now.

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,48 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 
 	"github.com/biboyboy04/EyeNako-Server/cmd/api"
+	"github.com/biboyboy04/EyeNako-Server/config"
+	"github.com/biboyboy04/EyeNako-Server/db"
+	"github.com/go-sql-driver/mysql"
 )
 
 func main() {
 	fmt.Println("Hello Go")
 
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User: config.Envs.DBUser,
+		Passwd: config.Envs.DBPassword,
+		Addr: config.Envs.DBAddress,
+		DBName: config.Envs.DBName,
+		Net: "tcp",
+		AllowNativePasswords: true,
+		ParseTime: true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Check if db is working/connected
+	initStorage(db)
+
+	//refactor, change to env for server addrs
 	addr := "localhost:5555"
-	server := api.NewAPIServer(addr, nil)
+	server := api.NewAPIServer(addr, db)
 
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
+
+func initStorage(db *sql.DB) {
+	err := db.Ping() 
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB Sucessfully Connected")
+} 
